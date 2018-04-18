2a7117a6d6a25d711351c058b014e484da3113f4
Christie Hong <christie.hong97@gmail.com>
2018-04-18 10:03:33 -0400

    few edits made to main.css, main.js - still working on css components of recipe page

--- main.js
+++ main.js
@@ -34,6 +34,7 @@ jQuery(document).ready(function($){
 	// add recipe to cart
 	$('#add-to-cart').on('click', function() {
 		event.preventDefault();
+		$('.cart-items').eq(0).find('p').remove();
 		var recipeAdded = $('<li class="recipe-name"><h3>Veggie Omelette</h3><i id="remove-recipe" class="fas fa-trash-alt"></i><ul class="ingredient"><li>olive oil</li><li>1 red bell pepper</li><li>1 onion</li><li>1 box of mushrooms</li><li>1 bag of spinach</li><li>8 eggs</li><li>salt</li><li>pepper</li></ul></li>');
 		$('.cart-items').eq(0).prepend(recipeAdded);
 	});
