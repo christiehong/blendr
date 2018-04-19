59203834a98e66afa5d39d0da5f2184afe2e6b86
Annie <annieyw@mit.edu>
2018-04-19 00:24:28 -0400

    interactive tutorial mildly working

--- tutorial.js
+++ tutorial.js
@@ -1,2 +1,18 @@
-jQuery(document).ready(function($){
-});
+var slideIndex = 1 
+var customerName
+//showSlide(1)
+//window.onload = showSlide(1)
+
+function slide(n) {
+	showSlide(slideIndex += n)
+}
+
+function showSlide(n) {
+	var i 
+	var slides = document.getElementsByClassName("tutorial")
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slides[slideIndex-1].style.display = "inline"
+}
+
