fe6dc47960afe8431f88a05d03a6871ad49fa00b
Christie Hong <christie.hong97@gmail.com>
2018-04-28 15:37:52 -0400

    recipe of page is added to the cart

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-var recipeDictionary = {'Veggie Omelette':['olive oil', '1 red bell pepper', '1 onion', '1 box of mushrooms', '1 bag of baby spinach leaves', '8 eggs', 'salt', 'pepper'],
-												'Huevos Rancheros':['2 plum tomatoes', '1 onion', '1 jalapeno pepper', '1 bottle of Red Hot hot sauce', 'cumin', 'salt', 'pepper', 'olive oil', '1 can of black beans', '4 eggs', 'feta cheese', '1 bunch of cilantro'],
-												'Loaded Veggie Quiche':['1 unbaked pie crust', 'olive oil', '1 box of mushrooms', '1 tomato', '1 bunch of basil', 'salt', 'pepper', 'olive oil', 'milk', '4 eggs', 'Colby-Monterey Jack cheese'],
-												'Spinach Potato Frittata':['6 small red potatoes', 'olive oil', '1 bag of spinach', '1 head of garlic', '1 onion', '6 eggs', 'salt', 'pepper', 'milk', 'Cheddar cheese']
-											}
+var veggie_omelette = {'Veggie Omelette':['olive oil', '1 red bell pepper', '1 onion', '1 box of mushrooms', '1 bag of baby spinach leaves', '8 eggs', 'salt', 'pepper']}
+var huevos_rancheros = {'Huevos Rancheros':['2 plum tomatoes', '1 onion', '1 jalapeno pepper', '1 bottle of Red Hot hot sauce', 'cumin', 'salt', 'pepper', 'olive oil', '1 can of black beans', '4 eggs', 'feta cheese', '1 bunch of cilantro']}
+var veggie_quiche = {'Loaded Veggie Quiche':['1 unbaked pie crust', 'olive oil', '1 box of mushrooms', '1 tomato', '1 bunch of basil', 'salt', 'pepper', 'olive oil', 'milk', '4 eggs', 'Colby-Monterey Jack cheese']}
+var veggie_frittata = {'Spinach Potato Frittata':['6 small red potatoes', 'olive oil', '1 bag of spinach', '1 head of garlic', '1 onion', '6 eggs', 'salt', 'pepper', 'milk', 'Cheddar cheese']}
+
+var recipesInCart = [];
 
 jQuery(document).ready(function($){
 
@@ -41,17 +42,27 @@ jQuery(document).ready(function($){
 	console.log($('.cart-items li').length);
 
 	// if cart is empty, insert "Your shopping list is currently empty."
-	if ($('.cart-items li').length == 0) {
+	if ($('.cart-items li').length == 0 && recipesInCart.length == 0) {
 		$('#cart').eq(0).append("<p> Your shopping list is currently empty.");
 	}
 
 	// add recipe to cart
 	$('#add-to-cart').on('click', function() {
 		event.preventDefault();
-		console.log(window.location.pathname.split("/").pop());
+		var page = window.location.pathname.split("/").pop();
+
 		$('#cart').eq(0).find('p').remove();
-		var recipeAdded = $('<li class="recipe-name"><h3>Veggie Omelette</h3><i id="remove-recipe" class="fas fa-trash-alt"></i><ul class="ingredient"><li>olive oil</li><li>1 red bell pepper</li><li>1 onion</li><li>1 box of mushrooms</li><li>1 bag of spinach</li><li>8 eggs</li><li>salt</li><li>pepper</li></ul></li>');
-		$('.cart-items').eq(0).prepend(recipeAdded);
+		console.log(page);
+		if (page == "veggie_omelette.html") {
+			recipesInCart.push(veggie_omelette);
+		} else if (page == "huevos_rancheros.html") {
+			recipesInCart.push(huevos_rancheros);
+		} else if (page == "veggie_frittata.html") {
+			recipesInCart.push(veggie_frittata);
+		} else if (page == "veggie_quiche.html") {
+			recipesInCart.push(veggie_quiche);
+		}
+		addRecipes();
 	});
 
 	// remove recipe from cart
@@ -71,3 +82,17 @@ jQuery(document).ready(function($){
 	})
 
 });
+
+function addRecipes() {
+	for (var i=0; i < recipesInCart.length; i++) {
+		recipeName = Object.keys(recipesInCart[i])[0];
+		var recipeAdded = '<li class="recipe-name"><h3>' + recipeName + '</h3><i id="remove-recipe" class="fas fa-trash-alt"></i><ul class="ingredient">';
+		ingredients = (recipesInCart[i][recipeName]);
+		for (var j=0; j < ingredients.length; j++) {
+			recipeAdded += "<li>" + ingredients[j] + "</li>";
+		}
+		recipeAdded += "</ul></li>"
+	}
+	$('.cart-items').eq(0).prepend(recipeAdded);
+
+}
